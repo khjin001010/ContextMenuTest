52661751f3e2f77e9c5cffca975e7eaa2bd061e7
khjin001010 <khjin001010@naver.com>
2017-05-25 19:24:54 +0900

    #4 image selected

--- app/src/main/java/khjin001010/kr/hs/emirim/contextmenutest/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/contextmenutest/MainActivity.java
@@ -1,21 +1,32 @@
 package khjin001010.kr.hs.emirim.contextmenutest;
 
+import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linear;
+    Button but1, but2;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button but1 = (Button)findViewById(R.id.but1);
-        Button but2 = (Button)findViewById(R.id.but2);
-        registerForContextMenu(but1);
+        but1 = (Button)findViewById(R.id.but1);
+        but2 = (Button)findViewById(R.id.but2);
+        linear = (LinearLayout)findViewById(R.id.linear);
+        img = (ImageView) findViewById(R.id.img);
+        registerForContextMenu(but1); //버튼 롱 클릭 -> 콘텍스트 메뉴 등장
         registerForContextMenu(but2);
     }
 
@@ -33,5 +44,30 @@ public class MainActivity extends AppCompatActivity {
 
         if(v.getId()==R.id.but2)
             menuInflater.inflate(R.menu.menu_2, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+
+        switch(item.getItemId()){
+            case R.id.menu1_item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu1_item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_pink:
+                img.setBackgroundColor(Color.rgb(255,253,233));
+                return true;
+            case R.id.menu2_item_rotation:
+                img.setRotation(but2.getRotation()+15);
+                return true;
+            case R.id.menu2_item_size:
+                img.setScaleX(linear.getScaleX()+1.5f);
+                img.setScaleY(linear.getScaleY()+1.5f);
+                return true;
+        }
+        return false;
     }
 }
